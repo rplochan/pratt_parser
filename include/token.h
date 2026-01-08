e016fc42d68fa8b11053934f25010662a9a629d3
rplochan <puttaiahr56@gmail.com>
2026-01-08 16:48:08 +0530

    token.h updated

--- include/token.h
+++ include/token.h
@@ -18,7 +18,6 @@ enum class TokenType {
 struct Token {
     TokenType type; // type_of_token
     std::string lexeme; // substring
-    double number = 0;
-    
+    double number = 0; // only for number tokens
 };
 
