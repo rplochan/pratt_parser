d1759d854aaf083c061cfcc31409e6428508a667
rplochan <puttaiahr56@gmail.com>
2026-01-08 16:31:43 +0530

    token.h updated

---
+++ include/token.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <string>
+
+enum class TokenType {
+    identifier,
+    number,
+    plus, //  +
+    minus, //  -
+    asterisk, //  *
+    slash, //  /
+    assign, //  =
+    left_paren, //  (
+    right_paren, //  )
+
+    EndOfFile
+}; 
+
+struct Token {
+    TokenType type; // type_of_token
+    std::string lexeme; // substring
+    double number = 0;
+    
+};
+
