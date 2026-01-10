4990ff353cffbf7b5d0da8bbebb18110df4e3061
rplochan <puttaiahr56@gmail.com>
2026-01-10 13:04:24 +0530

    lexer is implemented ie (lexer.cpp , lexer.h , token.h files are updated)

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -6,6 +6,7 @@
 #include<stdexcept>
 
 token lexer::get_next_token(){
+
     //skip white_space
 
     while(pos < input.length() && isspace(input[pos])){
@@ -18,8 +19,42 @@ token lexer::get_next_token(){
         return {token_type::EndOfFile, ""};
     }
 
+     char current_char = input[pos];
+
+     //numeric literal with decimal support
+
+    if(isdigit(current_char)){
+        std::string number_lexeme;
+        int dot_count =0;
+        while(pos < input.length() && (isdigit(input[pos])|| input[pos] == '.')){
+
+            if(input[pos] == '.'){
+                if(dot_count == 1){
+                    throw std::runtime_error("Invalid number format: multiple decimal points");
+                }
+                dot_count++;
+            }
+            
+            number_lexeme += input[pos++];
+        }
+
+        return {token_type::number, number_lexeme, std::stod(number_lexeme)};
+    }
+
+
+    //identifier (variable names)
+
+    if(isalpha(current_char) || current_char == '_'){
+        std::string identifier_lexeme;
+        while(pos < input.length() && ((isalnum(input[pos]) || input[pos] == '_'))){
+            identifier_lexeme += input[pos++];
+        }
+        return {token_type::identifier, identifier_lexeme};
+    }
+
+
     //single_character_tokens
-    char current_char = input[pos];
+
     switch(current_char){
         case '+':  
             pos++;
@@ -49,17 +84,22 @@ token lexer::get_next_token(){
             pos++;
             return {token_type::right_paren, ")"};
 
-        default:
-            throw std::runtime_error(std::string("Unexpected character: ") + current_char);
-
     }
 
-    
-    
-
-             
+    throw std::runtime_error(std::string("Unexpected character: ") + current_char);
 }
 
 
 
-std::vector<token> lexer::tokenize(){}
+std::vector<token> lexer::tokenize(){
+    std::vector<token> tokens;
+    while(true){
+        token t = get_next_token();
+        tokens.push_back(t);
+        if(t.type == token_type::EndOfFile){
+            break;
+        }
+    }
+
+    return tokens;
+}
